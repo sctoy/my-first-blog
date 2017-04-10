81906e3d8e9a6f933358a5e4ac2f7a5b239137b8
sctoy <toy@beema.com>
2017-04-10 12:43:56 -0400

    Added post detail template

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 # These two together represent and emptry string or the root URL. Homepage.
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
